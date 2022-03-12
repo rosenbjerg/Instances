3cbbebbc7cb4309cd40a7f51ae846cb27e4900ed
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2022-03-12 14:49:23 +0100

    Clean unused using directives

--- Instances/Instance.cs
+++ Instances/Instance.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
